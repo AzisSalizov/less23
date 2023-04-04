9a8ea16e810d62c7b497f6a0d8dd804e1e9e2b8f
Azis <azissalizov@gmail.com>
2023-04-04 15:36:47 +0600

    first commit

---
+++ ls.py
@@ -0,0 +1,66 @@
+# # telephone = {
+# #     'apple': 800,
+# #     'samsung':500,
+# #     'xiaomi': 400,
+# #     True:False
+# # }
+# # for i in telephone.keys():
+# #     print(i,end='')
+
+
+# # Создайте
+
+# cars_dict = [
+#     {
+#     'name': 'Lexus',
+#     'years':2020,
+#     },
+#       {
+#     'name': 'Mazda',
+#     'years':2020,
+#     }, 
+#     {
+#     'name': 'BMW',
+#     'years':2020,
+#     },
+#       {
+#     'name': 'Toyota',
+#     'years':2020,
+#     },
+# ]
+# for i in cars_dict:
+#     print(i['name'])
+
+
+# lst = [1,2,3,4,5,6,7,8,9,10]
+# print(lst)
+
+# lst_new = [i for i in range(0,11,2)]
+# print(lst_new)
+# 1,10 **3
+
+# {
+#     1:1,
+#     2:4,
+#     3:9,
+#     4:16
+# }
+
+# dct_new = {i: 'Z' for i in 'ABC'}
+# print(dct_new)
+
+
+
+# lst = [ "abcde", "123", "12345", "qwerty", "XXL" ]
+
+# lst_new = [i for i in lst if len(i) > 4]
+# print('resL =' , lst_new)
+
+# Вывод resL = ['abcde', '12345', 'qwerty']
+
+
+lst = [1,2,3,4,5,6]
+a = lst.index(2)
+print(a)
+# print(lst)
+
